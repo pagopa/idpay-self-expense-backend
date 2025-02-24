4eba3a7c59674aa8670cb8d0e484f28c3af93b97
stefanodel <stefano11.delia@nttdata.com>
2025-02-24 18:10:22 +0100

    Revert "fix:cors update"
    This reverts commit 5ba819350fe284dcf4336fe87c4656df09b46901.

--- src/main/java/it/gov/pagopa/self/expense/controller/WebviewControllerImpl.java
+++ src/main/java/it/gov/pagopa/self/expense/controller/WebviewControllerImpl.java
@@ -5,7 +5,6 @@ import it.gov.pagopa.self.expense.model.MilAuthAccessToken;
 import it.gov.pagopa.self.expense.service.WebviewService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -37,7 +36,6 @@ public class WebviewControllerImpl implements  WebviewController{
     }
 
     @Override
-    @CrossOrigin(origins = "https://welfare.uat.cstar.pagopa.it/")
     public Mono<ResponseEntity<MilAuthAccessToken>> session(String sessionId) {
         return webviewService.session(sessionId)
                 .map(ResponseEntity::ok);
