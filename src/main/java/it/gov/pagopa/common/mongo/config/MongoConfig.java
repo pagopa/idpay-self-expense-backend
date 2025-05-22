963618271af1ed0f1f67fc463957753ae3f89eea
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2025-05-22 16:04:59 +0200

    Update MongoConfig.java

--- src/main/java/it/gov/pagopa/common/mongo/config/MongoConfig.java
+++ src/main/java/it/gov/pagopa/common/mongo/config/MongoConfig.java
@@ -19,6 +19,7 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class MongoConfig {
 
+    
     @Configuration
     @ConfigurationProperties(prefix = "spring.data.mongodb.config")
     public static class MongoDbCustomProperties {
